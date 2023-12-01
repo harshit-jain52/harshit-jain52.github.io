84a6f0637660da87e2d833278b032435b5580be4
harshit-jain52 <harshitjain0502@gmail.com>
2023-12-01 17:02:43 +0530

    Dark/Light theme toggle working

---
+++ theme.js
@@ -0,0 +1,68 @@
+const loadTheme = theme => {
+    const root = document.querySelector(':root');
+    const darkLogos = document.querySelectorAll('.logo-dark');
+    const lightLogos = document.querySelectorAll('.logo-light');
+
+    root.setAttribute('color-scheme', `${theme}`);
+    localStorage.setItem('theme', `${theme}`);
+
+    if (theme == 'dark') {
+        darkLogos.forEach(
+            logo => {
+                logo.style.display = 'flex';
+                logo.classList.toggle('logo');
+            }
+        );
+
+        lightLogos.forEach(
+            logo => {
+                logo.style.display = 'none';
+                logo.classList.toggle('logo');
+            }
+        );
+
+        document.querySelector('.dark').style.display = 'flex';
+        document.querySelector('.light').style.display = 'none';
+    }
+    else {
+        darkLogos.forEach(
+            logo => {
+                logo.style.display = 'none';
+                logo.classList.toggle('logo');
+            }
+        );
+
+        lightLogos.forEach(
+            logo => {
+                logo.style.display = 'flex';
+                logo.classList.toggle('logo');
+            }
+        );
+
+        document.querySelector('.dark').style.display = 'none';
+        document.querySelector('.light').style.display = 'flex';
+    }
+}
+
+const getCurrentTheme = () => {
+    let theme = (matchMedia("(prefers-color-scheme: dark)").matches) ? 'dark' : 'light';
+    let local = localStorage.getItem('theme');
+    if (local) theme = local;
+    return theme;
+}
+
+currTheme = getCurrentTheme();
+loadTheme(currTheme);
+
+const btn = document.querySelector('#theme');
+btn.addEventListener('click', () => {
+    let theme = getCurrentTheme();
+    if (theme == 'dark') {
+        theme = 'light';
+        loadTheme(theme);
+    }
+    else {
+        theme = 'dark';
+        loadTheme(theme);
+    }
+});
