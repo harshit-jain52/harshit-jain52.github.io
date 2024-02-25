8bebbaba7f2e2b3c4f79c88cbd3308f28a913516
harshit-jain52 <harshitjain0502@gmail.com>
2024-02-25 16:18:54 +0530

    Code Formatting, color fixes

--- ratings.js
+++ ratings.js
@@ -1,58 +1,55 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("cur_rating").textContent = " Fetching...";
-    document.getElementById("max_rating").textContent = " Fetching...";
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("cur_rating").textContent = " Fetching...";
+  document.getElementById("max_rating").textContent = " Fetching...";
 
-    let handle = "harshit_jain52";
-    const api_url = "https://codeforces.com/api/user.rating?handle=" + handle;
+  let handle = "harshit_jain52";
+  const api_url = "https://codeforces.com/api/user.rating?handle=" + handle;
 
-    getapi(api_url).then(data => {
-        let len = data.result.length;
+  getapi(api_url)
+    .then((data) => {
+      let len = data.result.length;
 
-        if (len > 0) {
-            let cur_rating = data.result[len - 1].newRating;
+      if (len > 0) {
+        let cur_rating = data.result[len - 1].newRating;
 
-            let max_rating = 0;
-            for (let i = 0; i < len; i++) {
-                if (data.result[i].newRating > max_rating) max_rating = data.result[i].newRating;
-            }
-
-            const curR = document.getElementById("cur_rating");
-            curR.textContent = ` ${cur_rating}`;
-            curR.style.color = getColor(cur_rating);
-
-            const maxR = document.getElementById("max_rating");
-            maxR.textContent = ` ${max_rating}`;
-            maxR.style.color = getColor(max_rating);
+        let max_rating = 0;
+        for (let i = 0; i < len; i++) {
+          if (data.result[i].newRating > max_rating)
+            max_rating = data.result[i].newRating;
         }
-        else {
-            document.getElementById("cur_rating").parentElement.style.display = 'none'; // Unrated
-        }
-    }).catch(err => {
-        console.log("Rejected:", err.message);
+
+        const curR = document.getElementById("cur_rating");
+        curR.textContent = ` ${cur_rating}`;
+        curR.style.color = getColor(cur_rating);
+
+        const maxR = document.getElementById("max_rating");
+        maxR.textContent = ` ${max_rating}`;
+        maxR.style.color = getColor(max_rating);
+      } else {
+        document.getElementById("cur_rating").parentElement.style.display =
+          "none"; // Unrated
+      }
+    })
+    .catch((err) => {
+      console.log("Rejected:", err.message);
     });
 });
 
 async function getapi(url) {
-    const response = await fetch(url);
-    if(response.status !== 200){
-        throw new Error("cannot fetch the data");
-    }
-    const data = await response.json();
-    return data;
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    throw new Error("cannot fetch the data");
+  }
+  const data = await response.json();
+  return data;
 }
 
 function getColor(r) {
-    if (r < 1200)
-        return "gray";
-    if (r < 1400)
-        return "green";
-    if (r < 1600)
-        return "cyan";
-    if (r < 1900)
-        return "blue";
-    if (r < 2200)
-        return "violet";
-    if (r < 2400)
-        return "orange";
-    return "red";
+  if (r < 1200) return "gray";
+  if (r < 1400) return "green";
+  if (r < 1600) return "cyan";
+  if (r < 1900) return "blue";
+  if (r < 2200) return "violet";
+  if (r < 2400) return "orange";
+  return "red";
 }
--- theme.js
+++ theme.js
@@ -1,66 +1,56 @@
-const loadTheme = theme => {
-    const root = document.querySelector(':root');
-    const darkLogos = document.querySelectorAll('.logo-dark');
-    const lightLogos = document.querySelectorAll('.logo-light');
+const loadTheme = (theme) => {
+  const root = document.querySelector(":root");
+  const darkLogos = document.querySelectorAll(".logo-dark");
+  const lightLogos = document.querySelectorAll(".logo-light");
 
-    root.setAttribute('color-scheme', `${theme}`);
-    localStorage.setItem('hjpf.theme', `${theme}`);
+  root.setAttribute("color-scheme", `${theme}`);
+  localStorage.setItem("hjpf.theme", `${theme}`);
 
-    if (theme == 'dark') {
-        darkLogos.forEach(
-            logo => {
-                logo.style.display = 'flex';
-            }
-        );
+  if (theme == "dark") {
+    darkLogos.forEach((logo) => {
+      logo.style.display = "flex";
+    });
 
-        lightLogos.forEach(
-            logo => {
-                logo.style.display = 'none';
+    lightLogos.forEach((logo) => {
+      logo.style.display = "none";
+    });
 
-            }
-        );
+    document.querySelector(".dark").style.display = "flex";
+    document.querySelector(".light").style.display = "none";
+  } else {
+    darkLogos.forEach((logo) => {
+      logo.style.display = "none";
+    });
 
-        document.querySelector('.dark').style.display = 'flex';
-        document.querySelector('.light').style.display = 'none';
-    }
-    else {
-        darkLogos.forEach(
-            logo => {
-                logo.style.display = 'none';
+    lightLogos.forEach((logo) => {
+      logo.style.display = "flex";
+    });
 
-            }
-        );
-
-        lightLogos.forEach(
-            logo => {
-                logo.style.display = 'flex';
-            }
-        );
-
-        document.querySelector('.dark').style.display = 'none';
-        document.querySelector('.light').style.display = 'flex';
-    }
-}
+    document.querySelector(".dark").style.display = "none";
+    document.querySelector(".light").style.display = "flex";
+  }
+};
 
 const getCurrentTheme = () => {
-    let theme = (matchMedia("(prefers-color-scheme: dark)").matches) ? 'dark' : 'light';
-    let local = localStorage.getItem('hjpf.theme');
-    if (local) theme = local;
-    return theme;
-}
+  let theme = matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+  let local = localStorage.getItem("hjpf.theme");
+  if (local) theme = local;
+  return theme;
+};
 
 currTheme = getCurrentTheme();
 loadTheme(currTheme);
 
-const btn = document.querySelector('#theme');
-btn.addEventListener('click', () => {
-    let theme = getCurrentTheme();
-    if (theme == 'dark') {
-        theme = 'light';
-        loadTheme(theme);
-    }
-    else {
-        theme = 'dark';
-        loadTheme(theme);
-    }
+const btn = document.querySelector("#theme");
+btn.addEventListener("click", () => {
+  let theme = getCurrentTheme();
+  if (theme == "dark") {
+    theme = "light";
+    loadTheme(theme);
+  } else {
+    theme = "dark";
+    loadTheme(theme);
+  }
 });
