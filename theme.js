92e2ec1e569f1a20fdf11ca515f7ecb7585bc01c
harshit-jain52 <harshitjain0502@gmail.com>
2024-05-02 03:33:08 +0530

    hide CMake from top langs

--- theme.js
+++ theme.js
@@ -10,7 +10,7 @@ const loadTheme = (theme) => {
   if (theme == "dark") {
     if (langsCard) {
       langsCard.src =
-        "https://github-readme-stats.vercel.app/api/top-langs/?username=harshit-jain52&hide_progress=true&langs_count=10&theme=algolia";
+        "https://github-readme-stats.vercel.app/api/top-langs/?username=harshit-jain52&hide_progress=true&hide=cmake&langs_count=10&theme=algolia";
     }
 
     darkLogos.forEach((logo) => {
@@ -26,7 +26,7 @@ const loadTheme = (theme) => {
   } else {
     if (langsCard) {
       langsCard.src =
-        "https://github-readme-stats.vercel.app/api/top-langs/?username=harshit-jain52&hide_progress=true&langs_count=10&theme=buefy";
+        "https://github-readme-stats.vercel.app/api/top-langs/?username=harshit-jain52&hide_progress=true&hide=cmake&langs_count=10&theme=buefy";
     }
 
     darkLogos.forEach((logo) => {
