8f4a59e782c8760e51ec1b0092efccfde3d02061
harshit-jain52 <harshitjain0502@gmail.com>
2024-05-24 01:38:34 +0530

    Timeline init

---
+++ timeline.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  getProjects()
+    .then((data) => {
+      const table = document.querySelector(".timeline");
+      table.innerHTML = "";
+      data.forEach((project) => {
+        let html = `<div class="container">
+        <img
+            class="logo logo-dark"
+            src=${project.image.dark}
+            alt=${project.alt}
+        />
+        <img
+            class="logo logo-light"
+            src=${project.image.light}
+            alt=${project.alt}
+        />
+        <div class="textbox">
+            <h2>${project.name}</h2>
+            <small>2024</small>
+            <p>${project.description}</p> 
+        </div>
+      </div>`;
+
+        table.innerHTML = table.innerHTML + html;
+      });
+
+      loadTheme(getCurrentTheme());
+    })
+    .catch((err) => {
+      console.log("Rejected:", err.message);
+    });
+});
+
+async function getProjects() {
+  const response = await fetch("./projects.json");
+  if (response.status !== 200) {
+    throw new Error("cannot fetch the data");
+  }
+  const data = await response.json();
+  return data;
+}
